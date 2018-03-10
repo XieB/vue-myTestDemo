d02b41c8454f5e11d7363af65cae54fab0c6b7ef
谢保坤 <1410402563@qq.com>
2018-03-10 16:44:29 +0800

    vue第一次用

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Post from '@/components/Post'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Post',
+      component: Post,
+      meta: {
+        title: '模拟请求'
+      }
+    }
+  ]
+})
